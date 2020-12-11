65c4b42ca718423176e2711a1881ff213000df2f
jikeMisma <2889479657@qq.com>
2020-12-11 23:40:50 +0800

    整合课程评论接口

---
+++ api/commonedu.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+export default {
+    getPageList(page, limit, courseId) {
+    return request({
+      url: `/eduservice/comment/${page}/${limit}`,
+      method: 'get',
+      params: {courseId}
+    })
+  }
+}
--- pages/course/_id.vue
+++ pages/course/_id.vue
@@ -12,7 +12,7 @@
       <div>
         <article class="c-v-pic-wrap" style="height: 357px;">
           <section class="p-h-video-box" id="videoPlay">
-            <img :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
+            <img height="357px" width="100%" :src="courseWebVo.cover" :alt="courseWebVo.title" class="dis c-v-pic">
           </section>
         </article>
         <aside class="c-attr-wrap">
@@ -156,11 +156,99 @@
       </div>
     </section>
     <!-- /课程详情 结束 -->
+
+    <div class="mt50 commentHtml"><div>
+      <h6 class="c-c-content c-infor-title" id="i-art-comment">
+        <span class="commentTitle">课程评论</span>
+      </h6>
+      <section class="lh-bj-list pr mt20 replyhtml">
+        <ul>
+          <li class="unBr">
+            <aside class="noter-pic">
+              <img width="50" height="50" class="picImg" src="~/assets/img/avatar-boy.gif">
+              </aside>
+            <div class="of">
+              <section class="n-reply-wrap">
+                <fieldset>
+                  <textarea name="" v-model="comment.content" placeholder="输入您要评论的文字" id="commentContent"></textarea>
+                </fieldset>
+                <p class="of mt5 tar pl10 pr10">
+                  <span class="fl "><tt class="c-red commentContentmeg" style="display: none;"></tt></span>
+                  <input type="button" @click="addComment()" value="回复" class="lh-reply-btn">
+                </p>
+              </section>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section class="">
+          <section class="question-list lh-bj-list pr">
+            <ul class="pr10">
+              <li v-for="(comment,index) in data.items" v-bind:key="index">
+                  <aside class="noter-pic">
+                    <img width="50" height="50" class="picImg" :src="comment.avatar">
+                    </aside>
+                  <div class="of">
+                    <span class="fl"> 
+                    <font class="fsize12 c-blue"> 
+                      {{comment.nickname}}</font>
+                    <font class="fsize12 c-999 ml5">评论：</font></span>
+                  </div>
+                  <div class="noter-txt mt5">
+                    <p>{{comment.content}}</p>
+                  </div>
+                  <div class="of mt5">
+                    <span class="fr"><font class="fsize12 c-999 ml5">{{comment.gmtCreate}}</font></span>
+                  </div>
+                </li>
+              
+              </ul>
+          </section>
+        </section>
+        <!-- 公共分页 开始 -->
+        <div class="paging">
+            <!-- undisable这个class是否存在，取决于数据属性hasPrevious -->
+            <a
+            :class="{undisable: !data.hasPrevious}"
+            href="#"
+            title="首页"
+            @click.prevent="gotoPage(1)">首</a>
+            <a
+            :class="{undisable: !data.hasPrevious}"
+            href="#"
+            title="前一页"
+            @click.prevent="gotoPage(data.current-1)">&lt;</a>
+            <a
+            v-for="page in data.pages"
+            :key="page"
+            :class="{current: data.current == page, undisable: data.current == page}"
+            :title="'第'+page+'页'"
+            href="#"
+            @click.prevent="gotoPage(page)">{{ page }}</a>
+            <a
+            :class="{undisable: !data.hasNext}"
+            href="#"
+            title="后一页"
+            @click.prevent="gotoPage(data.current+1)">&gt;</a>
+            <a
+            :class="{undisable: !data.hasNext}"
+            href="#"
+            title="末页"
+            @click.prevent="gotoPage(data.pages)">末</a>
+            <div class="clear"/>
+        </div>
+        <!-- 公共分页 结束 -->
+      </div>
+    </div>
+
   </div>
+
+
 </template>
 
 <script>
 import courseApi from '@/api/course'
+import comment from '@/api/commonedu'
 export default {
    asyncData({ params, error }) {
      return courseApi.getBaseCourseInfo(params.id)
@@ -170,7 +258,53 @@ export default {
             chapterVideoList: response.data.data.chapterVideoList
           }
         })
-   }
+   },
+
+    data() {
+    return {
+      data:{},
+      page:1,
+      limit:4,
+      total:10,
+      comment:{
+        content:'',
+        courseId:''
+      },
+      courseInfo:{},
+      chapterVideoList:[],
+      isbuyCourse:false
+    }
+  },
+
+  created() {
+    this.initCourseInfo()
+    this.initComment()
+  },
+
+  methods:{
+    //获取课程详情
+    initCourseInfo() {
+      courseApi.getBaseCourseInfo(this.courseId)
+            .then(response => {
+              this.courseInfo=response.data.data.courseFrontInfo
+              this.chapterVideoList=response.data.data.chapterVideoList
+              this.isbuyCourse=response.data.data.isbuyCourse
+            })
+    },
+
+    initComment(){
+       comment.getPageList(this.page, this.limit, this.courseId).then(response => {
+           this.data = response.data.data
+       })
+    },
+
+    gotoPage(page){
+          comment.getPageList(page, this.limit,this.courseId).then(response => {
+              this.data = response.data.data
+          })
+      }
+  }
+  
 };
 </script>
 
